2597221e7abefd1fce4f3fd75aa3c210e80d77dd
Lamiaa Selim <lamiaa.selim@cts-egy.com>
2023-10-02 14:45:07 +0300

    setup-angular material

--- ClientSide/src/app/Component/admission/admission.component.ts
+++ ClientSide/src/app/Component/admission/admission.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-admission',
   templateUrl: './admission.component.html',
-  styleUrls: ['./admission.component.scss']
+  styleUrls: ['./admission.component.scss'],
+
 })
 export class AdmissionComponent {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 
+  constructor
+  (
+    private _formBuilder: FormBuilder
+  ) {}
 }
--- ClientSide/src/app/app.module.ts
+++ ClientSide/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './Component/log-in/log-in.component';
@@ -10,7 +10,13 @@ import { AboutComponent } from './Component/about/about.component';
 import { EventsComponent } from './Component/events/events.component';
 import { ContactUsComponent } from './Component/contact-us/contact-us.component';
 import { AdmissionComponent } from './Component/admission/admission.component';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +31,17 @@ import { AdmissionComponent } from './Component/admission/admission.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatStepperModule,
+    RouterLinkActive,
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
